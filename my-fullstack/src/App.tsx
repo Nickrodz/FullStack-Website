9b66b544899f119328a8f93c2a868e79902c236b
Nicholas Rodriguez <nickrodz277@gmail.com>
2025-06-27 14:26:42 -0700

    yolo

--- my-fullstack/src/App.tsx
+++ my-fullstack/src/App.tsx
@@ -31,6 +31,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>{backendData}</p>
+        <p>YOLO!</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
