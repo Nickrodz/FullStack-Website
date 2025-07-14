a96fc185f3cd4837a76ccaa5f76fb95778e707e8
Nicholas Rodriguez <nickrodz277@gmail.com>
2025-07-13 19:48:01 -0700

    Added some changes and started basic website

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
@@ -7,7 +7,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
+app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
 
